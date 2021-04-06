76fdde600986fd563669825e83ec0f604ec0c733
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2021-04-06 20:53:54 -0300

    get the square end

--- get-the-square/get-the-square.cpp
+++ get-the-square/get-the-square.cpp
@@ -30,9 +30,32 @@ struct object_t {
 
 static object_t *player;
 
+static object_t *forrest;
+
 static object_t *objs[MAX_OBJS];
 
 static int nobjs = 0;
+
+enum class game_state_t {
+	before,
+	running,
+	finished
+};
+
+game_state_t game_state;
+
+std::default_random_engine rgenerator;
+std::uniform_real_distribution<double> rdistribution(0.0,1.0);
+
+double get_random_0_1 ()
+{
+	return rdistribution(rgenerator);
+}
+
+double get_random (double max)
+{
+	return ((rdistribution(rgenerator) - 0.5) * 2.0 * max);
+}
 
 static void add_obj (object_t *o)
 {
@@ -42,18 +65,24 @@ static void add_obj (object_t *o)
 
 static void render ()
 {
+	int i;
+	object_t *o;
 	SDL_Rect rect;
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
-	rect.x = player->x;
-	rect.y = player->y;
-	rect.w = player->size;
-	rect.h = player->size;
+	for (i=0; i<nobjs; i++) {
+		o = objs[i];
+		
+		rect.x = o->x;
+		rect.y = o->y;
+		rect.w = o->size;
+		rect.h = o->size;
 
-	SDL_SetRenderDrawColor(renderer, player->r, player->g, player->b, 255);
-	SDL_RenderFillRect(renderer, &rect);
+		SDL_SetRenderDrawColor(renderer, o->r, o->g, o->b, 255);
+		SDL_RenderFillRect(renderer, &rect);
+	}
 
 	SDL_RenderPresent(renderer);
 }
@@ -79,6 +108,66 @@ static void check_collision_boundaries (object_t *o)
 	}
 }
 
+inline int check_rect_horizontal_collision (object_t *left, object_t *right)
+{
+	return ((left->x + left->size) >= right->x);
+}
+
+inline int check_rect_vertical_collision (object_t *top, object_t *bottom)
+{
+	return ((top->y + top->size) >= bottom->y);
+}
+
+int check_obj_collision (object_t *a, object_t *b)
+{
+	int r = 0;
+
+	if (a->x < b->x && check_rect_horizontal_collision(a, b)) {
+		if (a->y < b->y && check_rect_vertical_collision(a, b))
+			r = 1;
+		else if (a->y >= b->y && check_rect_vertical_collision(b, a))
+			r = 1;
+	}
+	else if (a->x >= b->x && check_rect_horizontal_collision(b, a)) {
+		if (a->y < b->y && check_rect_vertical_collision(a, b))
+			r = 1;
+		else if (a->y >= b->y && check_rect_vertical_collision(b, a))
+			r = 1;
+	}
+
+	return r;
+}
+
+void objs_collided (object_t *a, object_t *b)
+{
+	a->r = 0;
+	a->g = 0;
+	a->b = 255;
+
+	b->r = 0;
+	b->g = 0;
+	b->b = 255;
+
+	game_state = game_state_t::finished;
+}
+
+void physics_check_collisions ()
+{
+	int i, j;
+	object_t *a, *b;
+
+	for (i=0; i<nobjs; i++) {
+		a = objs[i];
+
+		for (j=i+1; j<nobjs; j++) {
+			b = objs[j];
+
+			if (check_obj_collision(a, b))
+				objs_collided(a, b);
+		}
+	}
+}
+
 static void physics (double t)
 {
 	int i;
@@ -91,13 +180,36 @@ static void physics (double t)
 
 		check_collision_boundaries(o);
 	}
+
+	physics_check_collisions();
+}
+
+inline double absf (double v)
+{
+	return (v >= 0.0) ? v : (v * -1.0);
+}
+
+static void run_forrest ()
+{
+	double dx = forrest->x - player->x;
+	double dy = forrest->y - player->y;
+
+	forrest->vx += dx / 15.0;
+	forrest->vy += dy / 15.0;
+
+	if (absf(dx) <= 100.0 && absf(dy) <= 100.0) {
+		if (get_random_0_1() <= 1.0) {
+			forrest->vx += get_random(100.0);
+			forrest->vy += get_random(100.0);
+		}
+	}
 }
 
 int main (int argc, char **argv)
 {
 	SDL_Event event;
 	const Uint8 *keyboard_state_array;
-	double elapsed, ds, speed, fps;
+	double elapsed, fps;
 	chrono::high_resolution_clock::time_point tbegin, tend;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -124,11 +236,12 @@ int main (int argc, char **argv)
 
 	keyboard_state_array = SDL_GetKeyboardState(NULL);
 
+	elapsed = 0.0;
 	fps = 100;
 
-	player = new object_t;
+	game_state = game_state_t::before;
 
-	add_obj(player);
+	player = new object_t;
 
 	player->x = SCREEN_W / 2;
 	player->y = SCREEN_H / 2;
@@ -139,7 +252,20 @@ int main (int argc, char **argv)
 	player->g = 0;
 	player->b = 0;
 
-	speed = 400;
+	add_obj(player);
+
+	forrest = new object_t;
+
+	forrest->x = SCREEN_W / 4;
+	forrest->y = SCREEN_H / 2;
+	forrest->vx = 0.0;
+	forrest->vy = 0.0;
+	forrest->size = 30;
+	forrest->r = 0;
+	forrest->g = 255;
+	forrest->b = 0;
+
+	add_obj(forrest);
 
 	while (alive) {
 		tbegin = chrono::high_resolution_clock::now();
@@ -151,25 +277,41 @@ int main (int argc, char **argv)
 
 		double ac = 100.0;
 
-		if (keyboard_state_array[SDL_SCANCODE_UP])
-			player->vy -= ac*elapsed;
-		if (keyboard_state_array[SDL_SCANCODE_DOWN])
-			player->vy += ac*elapsed;
+		if (game_state == game_state_t::running) {
+			if (keyboard_state_array[SDL_SCANCODE_UP])
+				player->vy -= ac*elapsed;
+			if (keyboard_state_array[SDL_SCANCODE_DOWN])
+				player->vy += ac*elapsed;
 
-		if (keyboard_state_array[SDL_SCANCODE_LEFT])
-			player->vx -= ac*elapsed;
-		if (keyboard_state_array[SDL_SCANCODE_RIGHT])
-			player->vx += ac*elapsed;
+			if (keyboard_state_array[SDL_SCANCODE_LEFT])
+				player->vx -= ac*elapsed;
+			if (keyboard_state_array[SDL_SCANCODE_RIGHT])
+				player->vx += ac*elapsed;
+			
+			run_forrest();
+
+			physics(elapsed);
+		}
 
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
 				case SDL_QUIT:
 					alive = 0;
 					break;
+
+				case SDL_KEYDOWN: {
+					switch (event.key.keysym.sym) {
+						case SDLK_SPACE: {
+							game_state = game_state_t::running;
+							break;
+						}
+					}
+
+					break;
+				}
 			}
 		}
 
-		physics(elapsed);
 		render();
 
 		do {
