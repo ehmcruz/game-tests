a04fb14d3aa22ae38f26b1255f4630141326be4b
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2023-11-13 10:40:19 -0300

    using SDL_Log to track prints in logcat and rendering the screen using the device native resolution

--- android-sdl-test-cmake-2/src/hello.cpp
+++ android-sdl-test-cmake-2/src/hello.cpp
@@ -2,9 +2,6 @@
 #include <SDL.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-
 int main(int argc, char* args[]) {
   SDL_Window* window = NULL;
   SDL_Surface* screenSurface = NULL;
@@ -12,11 +9,19 @@ int main(int argc, char* args[]) {
 	fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
 	return 1;
   }
+
+  SDL_DisplayMode displayMode;
+    if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0) {
+        SDL_Log("Erro ao obter o modo de exibição atual: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
   window = SDL_CreateWindow(
 				"hello_sdl2",
 				SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-				SCREEN_WIDTH, SCREEN_HEIGHT,
-				SDL_WINDOW_SHOWN
+				displayMode.w, displayMode.h,
+				SDL_WINDOW_FULLSCREEN
 				);
   if (window == NULL) {
 	fprintf(stderr, "could not create window: %s\n", SDL_GetError());
@@ -26,9 +31,16 @@ int main(int argc, char* args[]) {
  SDL_Renderer* renderer = NULL;
 	renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
 
+ SDL_SetRenderDrawColor( renderer, 255, 0, 255,  0);
+
+	// Clear winow
+	SDL_RenderClear( renderer );
+
+SDL_Delay(500);
+
 	Uint32 aa = SDL_GetTicks();
 	int n = 0;
-
+int i = 0;
 	while ((SDL_GetTicks() - aa) < 5000) {
 
  SDL_SetRenderDrawColor( renderer, 255, 0, 255,  0);
@@ -52,7 +64,10 @@ SDL_Rect r;
 
 	n += 10;
 
-  SDL_Delay(50);
+//	SDL_LogDebug(0, "TESTEEEEEEEEEEEEE %i\n", i);
+	SDL_Log("TESTEEEEEEEEEEEEE %i\n", i);   // use "adb logcat" to see this
+i++;
+  SDL_Delay(20);
 
 	}
  
